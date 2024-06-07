303bf3652c960b864c2dd15c04af45befc5d933e
harlee-x <hailin097@sina.com>
2024-06-07 17:48:38 +0800

    Fix: update kdp cmd version
    Signed-off-by: harlee-x <hailin097@sina.com>

--- cmd/const.go
+++ cmd/const.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,10 +16,10 @@ limitations under the License.
 package cmd
 
 const (
-	defaultKdpRepoUrl = "https://github.com/linktimecloud/kubernetes-data-platform.git"
-    defaultKdpRepoRef = "v1.1.0"
-    defaultArtifactServer = "https://registry.linktimecloud.com/repository/raw/vela"
-    defaultHelmRepoisotry = "oci://registry-cr.linktimecloud.com/linktimecloud/"
-    defaultDockerRegistry = "registry-cr.linktimecloud.com"
-	velaVersion = "1.9.9"
+	defaultKdpRepoUrl     = "https://github.com/linktimecloud/kubernetes-data-platform.git"
+	defaultKdpRepoRef     = "v1.1.1"
+	defaultArtifactServer = "https://registry.linktimecloud.com/repository/raw/vela"
+	defaultHelmRepoisotry = "oci://registry-cr.linktimecloud.com/linktimecloud/"
+	defaultDockerRegistry = "registry-cr.linktimecloud.com"
+	velaVersion           = "1.9.9"
 )
